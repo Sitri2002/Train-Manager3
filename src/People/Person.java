c766029e04864ad0ede44cf850d1e6420e6f659c
Jack Nguyen <nguyenduyanh241002@gmail.com>
2023-12-01 15:06:03 -0700

    Test

--- src/People/Person.java
+++ src/People/Person.java
@@ -1,9 +1,14 @@
 package People;
 
-import Software.*;
-import Hardware.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public abstract class Person {
+public abstract class Person implements Serializable {
 	private String name;
 	private String username;
 	private String password;
@@ -48,4 +53,48 @@ public abstract class Person {
 	public String getEmail() {
 		return email;
 	}
+	
+	public static void saveData(ArrayList<Person> p){
+		FileOutputStream fileOut = null;
+		ObjectOutputStream objOut= null;
+
+		try 
+		{
+			fileOut = new FileOutputStream("root/accounts.ser");		//the Employee object makes its way to serial data in the file Employee.ser
+			objOut = new ObjectOutputStream(fileOut);
+			objOut.writeObject(p);
+			objOut.close();
+			fileOut.close();
+	     }	
+		
+		catch(IOException i)
+	    {
+			i.printStackTrace();
+	    }		
+ 	}
+	
+	public static ArrayList<Person> loadData()
+	{	
+		FileInputStream fileIn = null;
+		ObjectInputStream objIn = null;
+		ArrayList<Person> p=null;
+			
+		try
+		{
+			fileIn = new FileInputStream("root/accounts.ser");
+			objIn = new ObjectInputStream(fileIn);
+			p = (ArrayList)objIn.readObject();
+			objIn.close();
+			fileIn.close();
+		}
+		catch(IOException i)
+		{
+			i.printStackTrace();
+		} 
+		catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}  
+		return p;
+	}	
 }
